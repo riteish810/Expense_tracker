0528913fc1fac156ea0716e481cafb1ec1641271
riteish810 <riteishgupta8108@gmail.com>
2025-05-12 17:02:56 +0530

    Updated UI

---
+++ lib/models/expense_item.dart
@@ -0,0 +1,45 @@
+import 'package:expense_tracker/models/expenses.dart';
+import 'package:flutter/material.dart';
+// import 'package:expense_tracker/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  final Expenses expenses; // <-- fixed type name
+
+  const ExpenseItem({super.key, required this.expenses});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15),
+        child: Column(
+          children: [
+            Text(
+              expenses.title, // <-- works now
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Text(
+                  '\$${expenses.amount.toStringAsFixed(2)}', // <-- works now
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(Icons.add),
+                    SizedBox(height: 10),
+                    Text(expenses.date.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/expense_list.dart
+++ lib/screens/expense_list.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense_item.dart';
 import 'package:expense_tracker/models/expenses.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -9,7 +10,7 @@ class ExpenseList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Text(expenses[index].title),
+      itemBuilder: (ctx, index) => ExpenseItem(expenses: expenses[index]),
     );
   }
 }
